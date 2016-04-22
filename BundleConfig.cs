f1384a060d7150a5f27e52fda91056ed482884c8
nant1086 <fadf810@gmail.com>
2016-04-22 11:12:35 -0500

    Added files via upload
    carga directa de scripts

---
+++ BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Optimization;
+using WebApplication1.Properties;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            string nombre, ruta, path = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "Scripts");
+            foreach (var item in Directory.GetFiles(path, "*.js", SearchOption.AllDirectories))
+            {
+                nombre = item.Substring(path.Length).Replace(@"\", "/");
+                ruta = HostingEnvironment.ApplicationPhysicalPath;
+                
+                if (Settings.Default.Minificar) // agrega bundles utilizando la estructura de carpetas y archivos como ruta virtual
+                {
+                    bundles.Add(new ScriptBundle("~/bundles" + nombre.Remove(nombre.Length - 3)).Include("~/" + item.Substring(ruta.Length).Replace(@"\", "/")));
+                }
+                else
+                {
+                    bundles.Add(new Bundle("~/bundles" + nombre.Remove(nombre.Length - 3)).Include("~/" + item.Substring(ruta.Length).Replace(@"\", "/")));
+                }
+            }
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js","~/Scripts/respond.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css","~/Content/site.css"));
+        }
+    }
+}
