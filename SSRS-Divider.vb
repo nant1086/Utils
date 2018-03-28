f6d2580b7dcd43e596d274dcc1dc87550aaba23c
nant1086 <fadf810@gmail.com>
2018-03-28 14:28:23 -0600

    Create SSRS-Divider.vb
    Valida si el denominador es cero para devolver nulo

---
+++ SSRS-Divider.vb
@@ -0,0 +1,13 @@
+Function Divide(Numerator as Double, Denominator as Double)
+  
+If Denominator = 0 Then
+  
+Return Nothing
+  
+Else
+  
+Return Numerator/Denominator
+  
+End If
+  
+End Function
